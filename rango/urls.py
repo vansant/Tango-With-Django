461034d8e36d327961f82f13aa9b2c2bd74fbba8
vansant <donny.vansant@gmail.com>
2014-06-15 12:55:38 -0700

    Completed Chapter 3 Exercises

--- rango/urls.py
+++ rango/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'rango.views.index', name='rango_index'),
+    url(r'^about/$', 'rango.views.about', name="rango_about"),
 )
--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    about_link = '<a href="/rango/about">About</a>'
+    return HttpResponse("Rango says: hello world!" + about_link)
+
+def about(request):
+    home_link = '<a href="/rango/">Home</a>'
+    return HttpResponse("Rango says: here is the about page." + home_link)
