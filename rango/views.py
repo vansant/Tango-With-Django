00a45e777340d32240d3f20d2eed6eb4a4ed395a
vansant <donny.vansant@gmail.com>
2014-06-15 13:55:40 -0700

    Updated whitespace

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,4 @@ def index(request):
     return render(request, 'rango/index.html', {'bold_message': 'This is the home page'})
 
 def about(request):
-  return render(request, 'rango/about.html', {'bold_message': 'This is the about page'})
+    return render(request, 'rango/about.html', {'bold_message': 'This is the about page'})
