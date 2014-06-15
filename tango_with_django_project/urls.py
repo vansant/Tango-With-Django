34d073e3ea8aea4972b23b45bd5d22efc82c829f
vansant <donny.vansant@gmail.com>
2014-06-15 15:09:48 -0700

    Configured development media server

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -17,3 +18,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+# Development Media Server
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+    (r'media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),)
